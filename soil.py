7ccca714aa5a6a8b50c72e8427cd1b1564c67b59
Jacob Asir <122958208+JacobAsir@users.noreply.github.com>
2025-10-25 11:50:20 +0530

    Update soil.py

--- soil.py
+++ soil.py
@@ -8,8 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load the model
-loaded_model = XGBClassifier()
-loaded_model.load_model("xgb_soil_analysis.bin")
+import joblib
+loaded_model = joblib.load("xgb_soil_analysis.bin")
+
 
 def get_data_JSON(relevant_data, model):
     """Convert sensor data to JSON format with prediction"""
